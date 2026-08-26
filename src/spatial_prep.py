cfaca79ab647257c77ca1d8cbbb411eb6e1e6fc3
kmiringu <75881550+kmiringu@users.noreply.github.com>
2026-08-26 11:48:16 +0300

    feat: draw zone boundary

--- src/spatial_prep.py
+++ src/spatial_prep.py
@@ -4,13 +4,22 @@ from shapely.geometry import LineString
 def generate_riparian_buffer_zone():
     print("step 1: Starting spatial geographic calculations...")
 
-    #  2. Create a fake river line to stimulate Nairobi  River coordinates
+    #  1. Create a fake river line to stimulate Nairobi  River coordinates
     #  download Nairobi river .geojson and gpd.read_file
 
     kasarani_river_gps_points = [(36.8219, -1.2921), (36.8350, -1.2850)],
     river_line_structure = LineString(kasarani_river_gps_points)
 
+    # 2. Put the line into a Geographic DataFrame(a smart map spreadsheet)
+    # we specify crs = "EPSG:4326", tells python these numbers are standard GPS Degrees
+    river_dataframe = gpd.GeoDataFrame(index=[0], crs="EPSG:4326", geometry=[river_line_structure])
+    print("Success: Loaded raw river coordinates in standard GPS Degrees")
+
     # 3. GIS transformation: Convert from degrees to Meters
     river_metric_dataframe = river_dataframe.to_crs(epsg=32737)
     print(" Success: Reprojected river tracking layer into real-world meters.")
+
+    # 4. Draw the legal zone boundary; now that our map is in meters
+    legal_60m_buffer_polygon = river_metric_dataframe.buffer(60)
+
 
